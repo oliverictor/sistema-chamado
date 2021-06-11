dbd96c753df6b2fd4391be950d14a09066fb6d14
Victor Oliveira <victor.soliveira@modalgr.com.br>
2021-06-11 16:30:43 -0300

    Projeto Criado

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyCZL9EVpbku-r_oBbkUD9NIIpvzvzk3Ehw",
+    authDomain: "sistema-f1512.firebaseapp.com",
+    projectId: "sistema-f1512",
+    storageBucket: "sistema-f1512.appspot.com",
+    messagingSenderId: "1005049086845",
+    appId: "1:1005049086845:web:02238a16e116c3505e4214",
+    measurementId: "G-BRZL58MT8L"
+};
+
+
+if (!firebase.app.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
