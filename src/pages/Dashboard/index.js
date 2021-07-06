53d5044a87f99cb32976ae8cf79285e2b5eceba2
Victor Oliveira <victor.soliveira@modalgr.com.br>
2021-07-06 20:30:04 -0300

    Cria tabela do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { FiMessageSquare, FiPlus } from "react-icons/fi";
+import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 import Header from "../../component/Header";
@@ -8,7 +8,7 @@ import Title from "../../component/Title";
 import './styles.css';
 
 export default function Dashboard() {
-    const [chamados, setChamados] = useState([]);
+    const [chamados, setChamados] = useState([1]);
 
     return (
         <div>
@@ -34,6 +34,37 @@ export default function Dashboard() {
                             <FiPlus size={25} color="#FFF" />
                             Novo chamado
                         </Link>
+
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Cliente</th>
+                                    <th scope="col">Assunto</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Scadastrado em</th>
+                                    <th scope="col">#</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td data-label="Cliente">Sujeito</td>
+                                    <td data-label="Assunto">Suporte</td>
+                                    <td data-label="Assunto">
+                                        <span className="badge" style={{ backgroundColor: '#5cb85c' }}>Em aberto</span>
+                                    </td>
+                                    <td data-label="Cadastrado">20/06/2021</td>
+                                    <td data-label="#">
+                                        <button className="action" style={{backgroundColor: '#3583f6'}}>
+                                            <FiSearch color="#FFF" size={17}/>
+                                        </button>
+
+                                        <button className="action" style={{backgroundColor: '#F6a935'}}>
+                                            <FiEdit2 color="#FFF" size={17}/>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </>
                 )}
             </div>
