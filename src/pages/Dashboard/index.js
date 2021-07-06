f03712a994425278d74eea9771246fb796735f35
Victor Oliveira <victor.soliveira@modalgr.com.br>
2021-07-05 23:25:27 -0300

    Cria tela de dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,42 @@
-import { useContext } from "react"
-import { AuthContext } from "../../contexts/auth"
+import { useState } from "react"
+import { FiMessageSquare, FiPlus } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 import Header from "../../component/Header";
+import Title from "../../component/Title";
+
+import './styles.css';
 
 export default function Dashboard() {
-
-    const { signOut } = useContext(AuthContext);
+    const [chamados, setChamados] = useState([]);
 
     return (
         <div>
             <Header />
 
-            <h1>Página Dashboard</h1>
-            <br /><br />
+            <div className="content">
+                <Title name="Atendimento">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                {chamados.length === 0 ? (
+                    <div className="container dashboard">
+                        <span>Nenhum registrado...</span>
+
+                        <Link to="/new" className="new">
+                            <FiPlus size={25} color="#FFF" />
+                            Novo chamado
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        <Link to="/new" className="new">
+                            <FiPlus size={25} color="#FFF" />
+                            Novo chamado
+                        </Link>
+                    </>
+                )}
+            </div>
         </div>
     )
 }
