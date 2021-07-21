1772ee8a99da9a5a61a2ff036538899081af3370
Victor Oliveira <victor.soliveira@modalgr.com.br>
2021-07-21 18:48:47 -0300

    Implementa funcionalidade de buscar dados para tabela do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,110 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
 import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { toast } from 'react-toastify';
 
 import Header from "../../component/Header";
 import Title from "../../component/Title";
 
+import firebase from '../../services/firebaseConnection';
+
+import { format } from 'date-fns';
+
 import './styles.css';
 
+const listRef = firebase.firestore().collection('chamados').orderBy('created', 'desc');
+
 export default function Dashboard() {
-    const [chamados, setChamados] = useState([1]);
+    const [chamados, setChamados] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(false);
+    const [lastDocs, setLastDocs] = useState();
+
+    async function loadChamados() {
+        await listRef.limit(5)
+            .get()
+            .then((snapshot) => {
+                updateState(snapshot)
+            })
+            .catch((err) => {
+                toast.error('Ops erro ao realizar esta operação, tente mais tarde.');
+                console.log('Erro ao buscar: ', err);
+                setLoadingMore(false);
+            })
+
+        setLoading(false)
+    }
+
+    async function updateState(snapshot) {
+        const isCollectionEmpty = snapshot.size === 0;
+
+        if (!isCollectionEmpty) {
+            let lista = [];
+
+            snapshot.forEach((doc) => {
+                lista.push({
+                    id: doc.id,
+                    assunto: doc.data().assunto,
+                    cliente: doc.data().cliente,
+                    clienteId: doc.data().clienteId,
+                    created: doc.data().created,
+                    createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+                    status: doc.data().status,
+                    complemento: doc.data().complemento
+                })
+            })
+
+            const lastDoc = snapshot.docs[snapshot.docs.length -1]; // Pega o ultimo documento buscado 
+
+            setChamados(chamados => [...chamados, ...lista]);
+            setLastDocs(lastDoc)
+
+        } else {
+            setIsEmpty(true);
+        }
+
+        setLoadingMore(false);
+    }
+
+    async function handleMore() {
+        setLoadingMore(true);
+        await listRef.startAfter(lastDocs).limit(5)
+        .get()
+        .then((snapshot) => {
+            updateState(snapshot)
+        })
+        .catch((err) => {
+            toast.error('Ops erro ao realizar esta operação, tente mais tarde.');
+            console.log('Erro ao buscar: ', err);
+            setLoadingMore(false);
+        })
+    }
+
+    useEffect(() => {
+        loadChamados();
+        return () => {
+
+        }
+    }, [])
+
+    if (loading) {
+        return (
+            <div>
+                <Header />
+                <div className="content">
+                    <Title name="Atendimento">
+                        <FiMessageSquare size={25} />
+                    </Title>
+
+                    <div className="container dashboard">
+                        <span>Buscando chamados...</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div>
@@ -46,25 +142,32 @@ export default function Dashboard() {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td data-label="Cliente">Sujeito</td>
-                                    <td data-label="Assunto">Suporte</td>
-                                    <td data-label="Assunto">
-                                        <span className="badge" style={{ backgroundColor: '#5cb85c' }}>Em aberto</span>
-                                    </td>
-                                    <td data-label="Cadastrado">20/06/2021</td>
-                                    <td data-label="#">
-                                        <button className="action" style={{backgroundColor: '#3583f6'}}>
-                                            <FiSearch color="#FFF" size={17}/>
-                                        </button>
+                                {chamados.map((item, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td data-label="Cliente">{item.cliente}</td>
+                                            <td data-label="Assunto">{item.assunto}</td>
+                                            <td data-label="Status">
+                                                <span className="badge" style={{ backgroundColor: item.status === 'Aberto' ? '#5cb85c' : '#999' }}>{item.status}</span>
+                                            </td>
+                                            <td data-label="Cadastrado">{item.createdFormated}</td>
+                                            <td data-label="#">
+                                                <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                                                    <FiSearch color="#FFF" size={17} />
+                                                </button>
 
-                                        <button className="action" style={{backgroundColor: '#F6a935'}}>
-                                            <FiEdit2 color="#FFF" size={17}/>
-                                        </button>
-                                    </td>
-                                </tr>
+                                                <button className="action" style={{ backgroundColor: '#F6a935' }}>
+                                                    <FiEdit2 color="#FFF" size={17} />
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
                             </tbody>
                         </table>
+
+                        {loadingMore && <h3 style={{ textAlign: 'center', marginTop: 15 }}>Buscando dados...</h3>}
+                        {!loadingMore && !isEmpty && <button className="btn-more" onClick={handleMore}>Buscar mais</button>}
                     </>
                 )}
             </div>
